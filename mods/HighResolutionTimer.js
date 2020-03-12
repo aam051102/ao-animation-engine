ea8c2df01cb3836696b5a704f5b67ea21e6829cc
4480135 <4480135@RTS-365.dk>
2020-03-12 21:36:41 +0100

    Modify high resolution timer.

--- mods/HighResolutionTimer.js
+++ mods/HighResolutionTimer.js
@@ -2,27 +2,15 @@ const HighResolutionTimer = window.HighResolutionTimer = window.HighResolutionTi
     const HighResolutionTimer = function(options) {
         this.timer = false;
 
-        this.total_ticks = 0;
-
-        this.start_time = undefined;
-        this.current_time = undefined;
-
         this.duration = (options.duration) ? options.duration : 1000;
         this.callback = (options.callback) ? options.callback : function() {};
 
         this.run = function() {
-            this.current_time = Date.now();
-            if (!this.start_time) { this.start_time = this.current_time; }
-            
-            
-            var nextTick = this.duration - (this.current_time - (this.start_time + (this.total_ticks * this.duration) ) );
-            this.total_ticks++;
-            
             (function(i) {
                 i.timer = setTimeout(function() {
                     i.callback(i);
                     i.run();
-                }, nextTick);
+                }, i.duration);
             }(this));
 
             return this;
